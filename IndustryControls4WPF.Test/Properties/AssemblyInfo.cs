d8c21358434520a6aa80820db58f007be95696e6
晓忠 郭 <guoxiaozhong1990@outlook.com>
2020-05-03 18:21:26 +0800

    初步完成转换

--- IndustryControls4WPF.Test/Properties/AssemblyInfo.cs
+++ IndustryControls4WPF.Test/Properties/AssemblyInfo.cs
@@ -1,20 +1,17 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("IndustryControls4WPF.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IndustryControls4WPF.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// [assembly: AssemblyTitle("IndustryControls4WPF.Test")]
+// [assembly: AssemblyDescription("")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("")]
+// [assembly: AssemblyProduct("IndustryControls4WPF.Test")]
+// [assembly: AssemblyCopyright("Copyright ©  2017")]
+// [assembly: AssemblyTrademark("")]
+// [assembly: AssemblyCulture("")]
 
 //将 ComVisible 设置为 false 将使此程序集中的类型
 //对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
@@ -51,5 +48,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyFileVersion("1.0.0.0")]
--- IndustryControls4WPF/Properties/AssemblyInfo.cs
+++ IndustryControls4WPF/Properties/AssemblyInfo.cs
@@ -1,20 +1,17 @@
-using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("IndustryControls4WPF")]
-[assembly: AssemblyDescription("工业测控程序组件")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Xiaozhong Guo (guoxiaozhong1990@outlook.com)")]
-[assembly: AssemblyProduct("IndustryControls4WPF")]
-[assembly: AssemblyCopyright("Copyright © 2001-2020 spartajet, All Rights Reserved")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// [assembly: AssemblyTitle("IndustryControls4WPF")]
+// [assembly: AssemblyDescription("工业测控程序组件")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("Xiaozhong Guo (guoxiaozhong1990@outlook.com)")]
+// [assembly: AssemblyProduct("IndustryControls4WPF")]
+// [assembly: AssemblyCopyright("Copyright © 2001-2020 spartajet, All Rights Reserved")]
+// [assembly: AssemblyTrademark("")]
+// [assembly: AssemblyCulture("")]
 
 //将 ComVisible 设置为 false 将使此程序集中的类型
 //对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
@@ -51,5 +48,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.4.0")]
-[assembly: AssemblyFileVersion("2.1.43.0")]
+// [assembly: AssemblyVersion("2.1.4.0")]
+// [assembly: AssemblyFileVersion("2.1.4.0")]
